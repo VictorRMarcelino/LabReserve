ea99cd775a8df651999b50de68ec964e22aeeabb
VictorRMarcelino <httpsvictorramos@gmail.com>
2025-10-13 21:39:24 -0300

    backend request

---
+++ backend/app/controller/ControllerUserRegister.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace app\controller;
+
+use routes\Request;
+
+/**
+ * Controller User Register
+ * @package app
+ * @subpackage controller
+ * @author Victor Ramos <httpsvictorramos@gmail.com>
+ * @since 13/10/2025
+ */
+class ControllerUserRegister {
+
+    /**
+     * Register a new user
+     * @param Request $request
+     * @return void
+     */
+    public function register(Request $request) {
+    }
+}
